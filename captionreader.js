f1ee384456dbdb11bfa0a11abc45fb7ab179efe0
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-12-19 11:46:40 +0530

    Basic extension functionalities commit

--- captionreader.js
+++ captionreader.js
@@ -1,29 +1,44 @@
-console.log('reader loaded');
+console.log("reader loaded");
 try {
-    // CNusmb
+  // CNusmb
+  let lwrSpeech = "";
+  const subtitleObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const newNodes = mutation.addedNodes;
+      newNodes.forEach((node) => {
+        if (node.classList && node.classList.contains("CNusmb")) {
+          const speech = node.textContent;
+
+          lwrSpeech = lwrSpeech + speech.toLowerCase();
+          console.log(lwrSpeech);
+        }
+      });
+    });
+  });
+
+  const refresh = setInterval(() => {
     const subtitleDiv = document.querySelector("div[jscontroller='D1tHje']");
-
-      const subtitleObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          const newNodes = mutation.addedNodes;
-
-          newNodes.forEach((node) => {
-            if (node.classList && node.classList.contains("CNusmb")) {
-              const speech = node.textContent;
-              
-              let lwrSpeech = speech.toLowerCase();
-
-              
-            }
-          });
-        });
+    subtitleObserver.observe(subtitleDiv, {
+      childList: true,
+      subtree: true,
+      attributes: false,
+      characterData: false,
+    });
+  }, 5000);
+  const sendRequest = setInterval(() => {
+      const text=lwrSpeech;
+      lwrSpeech="";
+    fetch("https://eaogudskckezrfywev.pythonanywhere.com/submit", {
+      method: "post",
+      body: JSON.stringify(text),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
       });
-      subtitleObserver.observe(subtitleDiv, {
-        childList: true,
-        subtree: true,
-        attributes: false,
-        characterData: false,
-      });
+  },30000);
 } catch (e) {
-    console.log('error in transcripting',e);
+  console.log("error in transcripting", e);
 }
