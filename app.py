d8e6ed2f46579336db03681c229acbcf88a5cc0b
vedangj044 <vedangj044@gmail.com>
2020-12-19 17:10:59 +0530

    Added ml algo

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, make_response, jsonify, Response
 import pandas as pd
 from flask_cors import CORS
+from mlsc import mls
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 data = pd.read_csv('https://firebasestorage.googleapis.com/v0/b/aarambh-aider.appspot.com/o/images%2Ffile.csv?alt=media', index_col=0)
+text = ""
 
 @app.route("/submit", methods=['GET', 'POST'])
 def home():
+    global text
+
     if request.method == 'POST':
+        text = request.get_json(force=True)['text']
+        print(text)
         return jsonify(message="Success", statusCode=201)
     else:
         return jsonify(message="use post", statusCode=200)
@@ -22,6 +28,11 @@ def leaderboard():
         "items": list(temp.to_dict(orient="Index").values())
     }
     return jsonify(resp)
+
+@app.route("/question")
+def question():
+    global text
+    return jsonify({"items": mls(text).getResponse()})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- ml.py
+++ ml.py
@@ -1,8 +1,10 @@
 from textblob import TextBlob
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from PyDictionary import PyDictionary
 
 text = "An integrated circuit or monolithic integrated circuit (also referred to as an IC, a chip, or a microchip) is a set of electronic circuits on one small flat piece (or \"chip\") of semiconductor material that is normally silicon. The integration of large numbers of tiny MOS transistors into a small chip results in circuits that are orders of magnitude smaller, faster, and less expensive than those constructed of discrete electronic components. The IC's mass production capability, reliability, and building-block approach to integrated circuit design has ensured the rapid adoption of standardized ICs in place of designs using discrete transistors. ICs are now used in virtually all electronic equipment and have revolutionized the world of electronics. Computers, mobile phones, and other digital home appliances are now inextricable parts of the structure of modern societies, made possible by the small size and low cost of ICs."
 
-# lines = text.replace(".", ".\n")
 sentences = text.split(".")
 
 noun = []
@@ -19,3 +21,43 @@ for i in blob.tags:
 
 print(set(noun))
 print(set(proper))
+
+for i in set(proper):
+    print(i, text.count(i))
+
+kl = set()
+
+final = []
+
+for i in set(proper):
+    for j in sentences:
+        if i in j:
+            if j not in kl:
+                final.append(j.replace(i, "_____"))
+                kl.add(j)
+
+print(random.choice(final))
+
+dictionary = PyDictionary()
+# print(blob.tags)
+gh = set()
+
+seg = []
+for i in blob.tags:
+    if i[1] == 'JJ':
+        if i not in gh:
+            gh.add(i[0])
+            ss = dictionary.antonym(i[0])
+            if ss is not None:
+                seg.append((i[0], ss[0]))
+
+sek = set()
+tr = []
+for i in sentences:
+    for j in seg:
+        if j[0] in i:
+            if i not in sek:
+                sek.add(i)
+                tr.append(i.replace(j[0], j[1]))
+
+print(tr)
